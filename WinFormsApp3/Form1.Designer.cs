539031ef3bbe880c36d20e29f15ed3ae0decba41
Haifaa Alsalmi <haifa.alsalmi@psdsarc.org.sa>
2025-01-16 08:57:44 +0300

    Add project files.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            textBox7 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(54, 222);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "load file";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(104, 496);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 25);
+            label1.TabIndex = 2;
+            label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 533);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Time Stamp";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(60, 580);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Azimuth";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(55, 624);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Elevation";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(76, 672);
+            label5.Name = "label5";
+            label5.Size = new Size(62, 25);
+            label5.TabIndex = 6;
+            label5.Text = "Range";
+            label5.Click += label5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ControlLight;
+            textBox2.Location = new Point(160, 496);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 7;
+            textBox2.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ControlLight;
+            textBox3.Location = new Point(160, 542);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 31);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ControlLight;
+            textBox4.Location = new Point(160, 580);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 31);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.ControlLight;
+            textBox5.Location = new Point(160, 624);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(150, 31);
+            textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.ControlLight;
+            textBox6.Location = new Point(160, 672);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(150, 31);
+            textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(147, 283);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 25);
+            label6.TabIndex = 12;
+            label6.Text = "file name:";
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = SystemColors.ControlLight;
+            textBox7.Location = new Point(241, 283);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(150, 31);
+            textBox7.TabIndex = 13;
+            textBox7.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(449, 760);
+            Controls.Add(textBox7);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label6;
+        private TextBox textBox7;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog of = new OpenFileDialog() { Filter = "Text Documents|*.txt", Multiselect = false, ValidateNames = true })
+                {
+                    if (of.ShowDialog() == DialogResult.OK)
+                    {
+                        // عرض اسم الملف في الليبل
+                        textBox7.Text = Path.GetFileName(of.FileName); // عرض اسم الملف 
+
+                        using (StreamReader sr = new StreamReader(of.FileName))
+                        {
+                            //  split 
+                            string content = await sr.ReadToEndAsync();
+                            string[] items = content.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                            // تعيين القيم في  Labels
+                            if (items.Length > 0) textBox2.Text = items.Length > 0 ? items[0].Trim() : "";
+                            if (items.Length > 1) textBox3.Text = items.Length > 1 ? items[1].Trim() : "";
+                            if (items.Length > 2) textBox4.Text = items.Length > 2 ? items[2].Trim() : "";
+                            if (items.Length > 3) textBox5.Text = items.Length > 3 ? items[3].Trim() : "";
+                            if (items.Length > 4) textBox6.Text = items.Length > 4 ? items[4].Trim() : "";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
